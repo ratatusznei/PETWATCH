82d71f52ba26d93c865f835f23d5f7163cd540fd
Pedro Sokulski <pedro.vicente.a.s@gmail.com>
2018-04-12 13:27:21 +0000

    Update all.h

--- código/all.h
+++ código/all.h
@@ -8,4 +8,16 @@
 #include <DS1307.h>
 #include "sprites.h"
 
+/*Funcoes do projeto*/
+/*Obrigado, Finger*/
+void escrever(char stringue[], short int tamanho ,short int tamanhoFonte, short int cor, short int posX, short int posY);
+char intToChar(int a);
+void displayStats();
+void displayBarras();
+void displayIdade();
+void storeAnything(char * thing, int tam, int pos);
+void readAnything(char * thing, int tam, int pos);
+void opcoesMenu(int opcao);
+void mostrarHoras();
+
 #endif /*__ALL_H*/
