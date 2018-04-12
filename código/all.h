69a8489595aa7fc8e7bd293eeea3d839d2ffb179
Pedro Sokulski <pedro.vicente.a.s@gmail.com>
2018-04-12 13:04:28 +0000

    Create all.h
    bibliotecas e coisinhas

---
+++ código/all.h
@@ -0,0 +1,11 @@
+#ifndef __ALL_H
+#define __ALL_H
+
+#include <Wire.h>
+#include <EEPROM.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <DS1307.h>
+#include "sprites.h"
+
+#endif /*__ALL_H*/
